e98f3a299f5bb3c12ef4aea0c569c24ac2315ab0
Yojan Shrestha <yshrestha@findhelp.com>
2022-01-06 08:46:38 -0600

    pr5

---
+++ event.js
@@ -0,0 +1,16 @@
+{
+  "pull_request": {
+    "event": {
+      "action": "labeled",
+      "label": {
+        "color": "d73a4a",
+        "default": true,
+        "description": "Something isn't working",
+        "id": 1519136641,
+        "name": "bug",
+        "node_id": "abcd",
+        "url": "https://api.github.com/repos/owner/repo/labels/bug"
+      }
+    }
+  }
+}
